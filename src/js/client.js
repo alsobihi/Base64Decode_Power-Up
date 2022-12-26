27647728f681f5ba3d2fa196a309270028fb0ee2
alsobihi <alsobihi.ai@gmail.com>
2022-12-26 20:17:52 +0300

    

--- src/js/client.js
+++ src/js/client.js
@@ -71,7 +71,7 @@ function cleanandreplace(decodedString) {
     var newstr = newstr.replace('', "@"); 
     var newstr = newstr.replace('', "@"); 
     var newstr = newstr.replace('', "@"); 
-    var newstr = newstr.replace('zzzzzzzzzzzz', "@"); 
+    var newstr = newstr.replace('', "@"); 
     var newstr = newstr.replace('zzzzzzzzzzzz', "@"); 
     
     console.log(newstr);
